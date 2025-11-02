00ce080f4b682dbe2c9bb9f61316058bb6e58186
Max Logvinov <maxlogvinov93@gmail.com>
2025-11-02 16:36:35 +0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'plan9-test-app',
+  webDir: 'dist'
+};
+
+export default config;
